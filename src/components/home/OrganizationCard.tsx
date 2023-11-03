175ade0ae7ab4c933b60caa5f26c8d4e57ed8276
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-11-03 21:09:57 +0530

    style: update header-footer and make home page responsive

--- src/components/home/OrganizationCard.tsx
+++ src/components/home/OrganizationCard.tsx
@@ -17,12 +17,11 @@ export const OrganizationCard = ({
 }: PropType) => {
   const MAX_DESCRIPTION_LENGTH = 180;
 
-  const filteredSkills = skills
-    ? skills.filter((skill) => skill !== "").slice(0, 6)
-    : [];
+  const filteredSkills = skills ? skills.filter((skill) => skill !== "") : [];
+
   return (
     <div className="box lg:w-[360px] sm:w-[360px] rounded-3xl bg-dark-blue mx-3 my-2 py-auto border-[3px] border-highlight-blue card hover:border-[5px] flex flex-col justify-center align-center overflow-hidden text-white">
-      <div className="flex flex-col justify-center items-center p-4 gap-5 mx-auto">
+      <div className="flex flex-col justify-center items-center p-4">
         {/* Logo */}
         <div className="flex justify-center items-center">
           <img
@@ -38,8 +37,8 @@ export const OrganizationCard = ({
         </h5>
 
         {/* Description */}
-        <div>
-          <p className="text-fade-grey text-sm text-center pt-[10px]">
+        <div className="m-1 p-1">
+          <p className="text-fade-grey text-sm text-center">
             {description.length > MAX_DESCRIPTION_LENGTH
               ? description.substring(0, MAX_DESCRIPTION_LENGTH) + "..."
               : description}
@@ -48,9 +47,9 @@ export const OrganizationCard = ({
 
         {/* Skills */}
         {filteredSkills.length > 0 && (
-          <div className="flex flex-col mx-auto pl-5">
-            <h2 className="text-white">Required Skills</h2>
-            <div className="flex flex-wrap items-center justify-self-center gap-[10px] pt-2">
+          <div className="flex flex-col p-1 m-1">
+            <h2 className="text-white text-center">Required Skills</h2>
+            <div className="flex flex-wrap items-center justify-evenly max-h-20 overflow-hidden gap-[10px] pt-2">
               {filteredSkills.map((skill) => (
                 <button
                   className="inline-flex items-center px-3 py-1 text-sm font-medium text-center text-white bg-highlight-blue rounded-3xl"
@@ -64,7 +63,7 @@ export const OrganizationCard = ({
         )}
 
         {/* Link to Organization page */}
-        <div className="flex space-x-3">
+        <div className="m-1 p-1">
           <a
             href={`/org/${id}`}
             className="viewDetail inline-flex items-center px-4 py-2 pb-6 text-sm font-medium text-center text-white bg-dark-blue hover:bg-white hover:text-dark-blue hover:border-highlight-blue"
